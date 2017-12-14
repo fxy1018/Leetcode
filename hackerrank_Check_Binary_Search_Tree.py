abf6013473c5004266e86b8a5da19647a29a569e
Shirley <xueyi.fan27@gmail.com>
2017-12-14 15:23:42 -0500

    Create hackerrank_Check_Binary_Search_Tree.py

---
+++ hackerrank_Check_Binary_Search_Tree.py
@@ -0,0 +1,11 @@
+###
+For the purposes of this challenge, we define a binary search tree to be a binary tree with the following ordering properties:
+
+The  value of every node in a node's left subtree is less than the data value of that node.
+The  value of every node in a node's right subtree is greater than the data value of that node.
+Given the root node of a binary tree, can you determine if it's also a binary search tree?
+
+Complete the function in your editor below, which has  parameter: a pointer to the root of a binary tree. It must return a boolean denoting whether or not the binary tree is a binary search tree. You may have to write one or more helper functions to complete this challenge.
+
+###
+
