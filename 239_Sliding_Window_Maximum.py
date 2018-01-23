32d03fff02382c6ba90bdef47162bb9eead4562a
Shirley <xueyi.fan27@gmail.com>
2018-01-23 11:14:35 -0500

    Create 239_Sliding_Window_Maximum.py

---
+++ 239_Sliding_Window_Maximum.py
@@ -0,0 +1,98 @@
+'''
+Created on Jan 18, 2018
+
+@author: XFan
+
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+For example,
+Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Therefore, return the max sliding window as [3,3,5,5,6,7].
+
+Note: 
+You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
+
+Follow up:
+Could you solve it in linear time?
+
+strategy: 
+1. heap
+优先队列
+复杂度
+时间 O(NlogK) 空间 O(K)
+
+思路
+维护一个大小为K的最大堆，依此维护一个大小为K的窗口，每次读入一个新数，都把堆中窗口最左边的数扔掉，再把新数加入堆中，这样堆顶就是这个窗口内最大的值。
+
+注意
+-结果数组的大小是nums.length + 1 - k， 赋值时下标也是i + 1 - k
+
+2. use two end array to store the index of an array
+
+双向队列
+复杂度
+时间 O(N) 空间 O(K)
+
+思路
+我们用双向队列可以在O(N)时间内解决这题。当我们遇到新的数时，将新的数和双向队列的末尾比较，如果末尾比新数小，则把末尾扔掉，直到该队列的末尾比新数大或者队列为空的时候才住手。这样，我们可以保证队列里的元素是从头到尾降序的，由于队列里只有窗口内的数，所以他们其实就是窗口内第一大，第二大，第三大...的数。保持队列里只有窗口内数的方法和上个解法一样，也是每来一个新的把窗口最左边的扔掉，然后把新的加进去。然而由于我们在加新数的时候，已经把很多没用的数给扔了，这样队列头部的数并不一定是窗口最左边的数。这里的技巧是，我们队列中存的是那个数在原数组中的下标，这样我们既可以直到这个数的值，也可以知道该数是不是窗口最左边的数。这里为什么时间复杂度是O(N)呢？因为每个数只可能被操作最多两次，一次是加入队列的时候，一次是因为有别的更大数在后面，所以被扔掉，或者因为出了窗口而被扔掉。
+'''
+
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if not nums or k<1 or len(nums) < k:
+            return([])
+        
+        qMax = []
+        res = []
+        for i in range(len(nums)):
+            while qMax and nums[qMax[-1]] <= nums[i]:
+                qMax.pop()
+            qMax.append(i) 
+            if qMax[0] == i-k:
+                qMax = qMax[1:]
+            if i >= k-1:
+                res.append(nums[qMax[0]])
+        return(res)
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
