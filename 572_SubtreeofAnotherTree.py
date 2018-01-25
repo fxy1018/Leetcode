e220e7f272eff982e1589d3e616db08b60545957
Shirley <xueyi.fan27@gmail.com>
2018-01-25 15:30:34 -0500

    Create 572_SubtreeofAnotherTree.py

---
+++ 572_SubtreeofAnotherTree.py
@@ -0,0 +1,61 @@
+'''
+
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false.
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+        if not s:
+            return(False)
+        return(self.isEqual(s,t) or self.isSubtree(s.left,t) or self.isSubtree(s.right,t))
+        
+    def isEqual(self, s, t):
+        if not s and not t:
+            return(True)
+        if not s or not t:
+            return(False)
+        return(s.val == t.val and self.isEqual(s.left, t.left) and self.isEqual(s.right,t.right))
+                
+      
