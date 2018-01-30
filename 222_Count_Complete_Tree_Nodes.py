c7569b5c974801dd9171f49e01b4c0c6745bd4eb
Shirley <xueyi.fan27@gmail.com>
2018-01-30 12:45:01 -0500

    Update 222_Count_Complete_Tree_Nodes.py

--- 222_Count_Complete_Tree_Nodes.py
+++ 222_Count_Complete_Tree_Nodes.py
@@ -68,9 +68,42 @@ class Solution2(object):
         
             
         
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution3(object):
+    def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        #method2: use the complete tree node 
+        if not root:
+            return(0)
+        hight = self.getLeftMostLevel(root)
+        return(self.helpFun(root, hight))
+    
+    def helpFun(self, node, hight):
+        if hight == 0:
+            return(0)
         
-        
-        
+        rightHight = self.getLeftMostLevel(node.right)
+        if hight == rightHight+1:
+            return(2**(hight-1) + self.helpFun(node.right, rightHight))
+        else:
+            return(2**(hight-2) + self.helpFun(node.left, hight-1)) 
+
+    
+    def getLeftMostLevel(self, node):
+        level = 1
+        while node:
+            level += 1
+            node = node.left
+        return(level-1)
         
         
         
