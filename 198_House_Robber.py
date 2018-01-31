474767b80d3ed9bbd632c535120c1737c581aaaf
Shirley <xueyi.fan27@gmail.com>
2018-01-31 13:34:20 -0500

    Update 198_House_Robber.py

--- 198_House_Robber.py
+++ 198_House_Robber.py
@@ -46,3 +46,23 @@ class Solution(object):
             v1 = tmp
         
         return(v2)
+    
+class Solution:
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #relation dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        if not nums:
+            return(0)
+        if len(nums) == 1:
+            return(nums[-1])
+        
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        
+        return(dp[-1])  
