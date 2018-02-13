16161acf99245f49b28d8ed0cf23718ccfd608f7
Shirley <xueyi.fan27@gmail.com>
2018-02-13 13:29:32 -0500

    Update 85_MaximalRectangle.py

--- 85_MaximalRectangle.py
+++ 85_MaximalRectangle.py
@@ -11,3 +11,40 @@ Return 6.
 
 '''
 
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        res = 0
+        if not matrix:
+            return(res)
+        row = len(matrix)
+        col = len(matrix[0])
+        newMatrix = [matrix[0]]
+        lastRow = [0] * col
+        for i in range(row):
+            newRow = []
+            for j in range(col):
+                if matrix[i][j] == "0":
+                    newRow.append(0)
+                else:
+                    newRow.append(lastRow[j]+1)
+            res = max(res, self.getMax(newRow))
+            lastRow = newRow
+        
+        return(res)
+            
+    def getMax(self, heights):
+        res = 0
+        stack = [-1]
+        heights.append(0)
+        n = len(heights)
+        for i in range(n):
+            curr = heights[i]
+            while stack[-1]!=-1 and heights[stack[-1]]>= curr:
+                index = stack.pop()
+                res = max(res, heights[index]*(i-stack[-1]-1))
+            stack.append(i)
+        return(res)
