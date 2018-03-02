5c1a96412162a717b576b9d596649876fd19db9f
Shirley <xueyi.fan27@gmail.com>
2018-03-02 12:28:54 -0500

    Create 692_TopK_FrequentWords.py

---
+++ 692_TopK_FrequentWords.py
@@ -0,0 +1,43 @@
+'''
+Given a non-empty list of words, return the k most frequent elements.
+
+Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
+
+Example 1:
+Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+Output: ["i", "love"]
+Explanation: "i" and "love" are the two most frequent words.
+    Note that "i" comes before "love" due to a lower alphabetical order.
+Example 2:
+Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+Output: ["the", "is", "sunny", "day"]
+Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+    with the number of occurrence being 4, 3, 2 and 1 respectively.
+Note:
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Input words contain only lowercase letters.
+Follow up:
+Try to solve it in O(n log k) time and O(n) extra space.
+
+'''
+
+#create maxHeap
+Time: N+klogN
+
+import heapq
+class Solution:
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        wordHash = {}
+        for w in words:
+            wordHash[w] = wordHash.get(w, 0) + 1
+        
+        #maxHeap
+        heap = [(-freq,word) for word, freq in wordHash.items()]
+        heapq.heapify(heap)
+        return([heapq.heappop(heap)[1] for _ in range(k)])
+        
