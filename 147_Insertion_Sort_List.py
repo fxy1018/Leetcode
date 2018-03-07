f0ad9b15d7fb20d12b38a5efb3a718991c83878d
Shirley <xueyi.fan27@gmail.com>
2018-03-07 15:27:00 -0500

    Update 147_Insertion_Sort_List.py

--- 147_Insertion_Sort_List.py
+++ 147_Insertion_Sort_List.py
@@ -48,6 +48,46 @@ class Solution(object):
     
                 
                 
+     # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#create a dummy head and pre pointer
+class Solution(object):
+    def insertionSortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return(head)
+        
+        dummy = ListNode(-1)
+        dummy.next = pre = head
+        node = head.next
+        
+        while node:
+            q1 = dummy
+            nextNode = node.next
             
+            if node.val < pre.val:
+                while q1.next and q1.next != node:
+                    if q1.next.val>=node.val:
+                        pre.next = nextNode
+                        tmp = q1.next
+                        q1.next = node
+                        node.next = tmp
+                        break
+                    else:
+                        q1 = q1.next
+                    
+            else:
+                pre = node
+            
+            node = nextNode
+                
+        return(dummy.next)       
             
         
