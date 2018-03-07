4f0d8e4c87cb28c212a117a74f8d95452e78b3e1
Shirley <xueyi.fan27@gmail.com>
2018-03-07 14:38:07 -0500

    Update 143_reorder_list.py

--- 143_reorder_list.py
+++ 143_reorder_list.py
@@ -57,6 +57,47 @@ class Solution(object):
             pre= node
             node = next
         return(pre)
+ 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: void Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+        
+        p1 = p2 = head
+        while p1 and p1.next:
+            p1 = p1.next.next
+            p2 = p2.next
+            
+        node = self.reverseList(p2.next)
+        p2.next = None
+        p3 = head
+        while node:
+            tmp = p3.next
+            tmp2 = node.next
+            p3.next = node
+            node.next = tmp
+            p3 = tmp
+            node = tmp2
+        
+    def reverseList(self, node):
+        pre = None
+        curr = node
+        while curr:
+            tmp = curr.next
+            curr.next = pre
+            pre = curr
+            curr = tmp
+        return(pre)
             
         
 node = ListNode(1)
