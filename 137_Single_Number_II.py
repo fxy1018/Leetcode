a8d73ad4c7edbcea977c13a71578267b54ca4e79
Shirley <xueyi.fan27@gmail.com>
2018-03-14 10:18:50 -0400

    Update 137_Single_Number_II.py

--- 137_Single_Number_II.py
+++ 137_Single_Number_II.py
@@ -14,7 +14,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """    
-        #bit manipulation
+        #bit manipulation: 充分利用数组元素是int类型这一特点，统计所有元素的第i个bit上为1的个数，因为题目说了其它元素出现了3次，而特殊元素只出现了1次，所以当统计的个数不能整除3就表明特殊元素在第i个bit上是1，所以我们把结果的第i个bit设为1，通过检查32个bit，这样我们就构造出了特殊元素，最后返回结果即可，时间复杂度是O（32n）
         ans = 0
         for i in range(32):
             sum = 0
