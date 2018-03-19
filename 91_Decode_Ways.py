00257a36b644b2628f80126580e2ac4d07d428da
Shirley <xueyi.fan27@gmail.com>
2018-03-19 10:45:44 -0400

    Update 91_Decode_Ways.py

--- 91_Decode_Ways.py
+++ 91_Decode_Ways.py
@@ -46,7 +46,63 @@ class Solution(object):
         return(dp[-1])
         
         
+class Solution:
+    #dp[i] record number of ways which must end at i index dp[i] = ?dp[i-1] + ?dp[i-2] 
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return(0)
+        l = len(s)
+        dp = [0] * (l+1)
+        dp[0] = 1
+        if s[0] == "0":
+            dp[1] = 0
+        else:
+            dp[1] = 1
+        
+        for i in range(1, l):
+            if s[i-1:i+1] == '00':
+                return(0)
+            elif 9 <int(s[i-1:i+1]) < 27:
+                dp[i+1] = dp[i-1]
+            
+            if s[i] != '0':
+                dp[i+1] += dp[i] 
 
+        return(dp[-1])
+#compress space 
+class Solution:
+    #dp[i] record number of ways which must end at i index dp[i] = ?dp[i-1] + ?dp[i-2] 
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s or s[0] == '0':
+            return(0)
+        pre = 1
+        prepre = 1
+        
+        for i in range(1, len(s)):
+            if s[i] != '0':
+                curr = pre
+            else:
+                curr = 0
+                
+            if s[i-1:i+1] == '00':
+                return(0)
+            
+            elif 9 <int(s[i-1:i+1]) < 27:
+                curr += prepre
+                
+            prepre = pre
+            pre = curr
+            
+
+        return(pre)
 
         
         
