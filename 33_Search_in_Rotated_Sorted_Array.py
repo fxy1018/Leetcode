3b0e813e8aea388142fb8adc042df219d7cd6747
Shirley <xueyi.fan27@gmail.com>
2018-03-23 13:48:25 -0400

    Update 33_Search_in_Rotated_Sorted_Array.py

--- 33_Search_in_Rotated_Sorted_Array.py
+++ 33_Search_in_Rotated_Sorted_Array.py
@@ -59,6 +59,76 @@ class Solution(object):
         else:
             return(-1)
                 
+   class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return(-1)
         
+        left = 0
+        right = len(nums)-1
         
- 
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return(mid)
+            
+            if nums[left] > nums[right]:
+                if nums[mid] < nums[right]:
+                    if nums[mid] > target:
+                        right = mid-1
+                        
+                    else:
+                        if nums[right] < target:
+                            right = mid-1
+                        else:
+                            left = mid + 1
+                else:
+                    if nums[mid] < target:
+                        left = mid+1
+                    else:
+                        if nums[left] > target:
+                            left = mid+1
+                        else:
+                            right = mid-1
+                        
+            else:
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid -1 
+                    
+        return(-1)     
+        
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return(-1)
+        
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return(mid)
+            if nums[mid] <= nums[right]:
+                if nums[mid] < target and target <=nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if nums[mid]>  target and target >= nums[left]:
+                    right = mid-1
+                else:
+                    left = mid+1
+        return(-1) 
