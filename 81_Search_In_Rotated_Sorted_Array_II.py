5eb95f95c941cef9d16c7fb8354f8dbd202b17d9
Shirley <xueyi.fan27@gmail.com>
2018-03-23 14:00:16 -0400

    Update 81_Search_In_Rotated_Sorted_Array_II.py

--- 81_Search_In_Rotated_Sorted_Array_II.py
+++ 81_Search_In_Rotated_Sorted_Array_II.py
@@ -77,4 +77,35 @@ class Solution(object):
                 right -= 1
                 
         return(False)
+ class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+       
+        if not nums:
+            return(False)
         
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return(True)
+            if nums[mid] < nums[right]:
+                if nums[mid] < target and target <=nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            elif nums[mid] > nums[right]:
+                if nums[mid]>  target and target >= nums[left]:
+                    right = mid-1
+                else:
+                    left = mid+1
+            else:
+                right -= 1
+                
+        return(False)       
