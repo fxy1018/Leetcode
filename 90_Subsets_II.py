4128006c2f17d7b3754073afe230b7e53b2a9043
Shirley <xueyi.fan27@gmail.com>
2018-03-23 15:37:00 -0400

    Update 90_Subsets_II.py

--- 90_Subsets_II.py
+++ 90_Subsets_II.py
@@ -54,5 +54,29 @@ class Solution(object):
             new_comb = comb + [self.nums[i]]
             self.helpFun(digits, index+1, new_comb, i+1)
             
+class Solution:
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums = sorted(nums)
+        res = [[]]
+        self._helpFun(nums,  res, [])
+        return(res)
+    
+    def _helpFun(self, nums, res, curr):
+        if not nums:
+            if curr not in res:
+                res.append(curr)
+            return
+        
+        for i in range(len(nums)):
+            newCurr = curr + [nums[i]]
+            if newCurr not in res:
+                res.append(newCurr)
+            
+            self._helpFun(nums[i+1:], res, newCurr )     
+            
 s = Solution()
 print(s.subsetsWithDup([4,4,4,1,4]))
