d14b790560833772eb9d7c7e14e43e40cbb13c69
Shirley <xueyi.fan27@gmail.com>
2018-03-29 13:01:51 -0400

    Update 131_Palindrome_Partitioning.py

--- 131_Palindrome_Partitioning.py
+++ 131_Palindrome_Partitioning.py
@@ -18,5 +18,34 @@ Created on Mar 16, 2017
 
 @author: fanxueyi
 '''
-
+class Solution:
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        if not s:
+            return([])
+        res = []
+        curr = []
+        palHash= set([]) 
+        self._helpFun(s, res,curr,palHash)
+        print(palHash)
+        return(res)
+    
+    def _helpFun(self, s, res, curr, palHash):
+        if not s:
+            res.append(curr)
+            return
+        
+        l = len(s)
+        for i in range(l):
+            part1 = s[0:i+1]
+            part2 = s[i+1:]
+            if part1 in palHash or part1 == part1[::-1]:
+                palHash.add(part1)
+                newCurr= curr + [part1]
+                self._helpFun(part2, res, newCurr, palHash) 
+            else:
+                continue
 
