558ddd29141f31648b701d21f303e857885e2a0e
Shirley <xueyi.fan27@gmail.com>
2018-03-30 10:40:46 -0400

    Create 179_LargestNumber.py

---
+++ 179_LargestNumber.py
@@ -0,0 +1,34 @@
+'''
+
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+
+Note: The result may be very large, so you need to return a string instead of an integer.
+
+将整型数组的转化为字符串数组，用 std::sort 对字符串数组排序，使得排序后元素直接连接成为最大值整数。
+
+使用默认的比较函数排序，会得到不一定是最大值，例如 {360, 36} 排序后得到的是 36036 ，明显不是小于 36360。解决这个问题，就是重写比较函数，使得排序后的两个元素连接成为较大的整数即可，很简洁。
+
+'''
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {string}
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        numStr = [str(n) for n in nums]
+        numStr = sorted(numStr, reverse = True, cmp=lambda x,y: cmp((x+y), (y+x)))
+
+        res = ''.join(numStr)
+        
+        #remove 0 at the begining
+        i= 0
+        while res[i]=='0' and i < len(nums)-1:
+            i+=1
+        
+        return(res[i:])
+    
