7c8b5c293e10ca03486a6c86e2dc8ff9c8dd19bc
Shirley <xueyi.fan27@gmail.com>
2018-03-30 10:05:11 -0400

    Update 166_Fraction_to_RecurringDecimal.py

--- 166_Fraction_to_RecurringDecimal.py
+++ 166_Fraction_to_RecurringDecimal.py
@@ -10,6 +10,8 @@ Given numerator = 1, denominator = 2, return "0.5".
 Given numerator = 2, denominator = 1, return "2".
 Given numerator = 2, denominator = 3, return "0.(6)".
 
+
+再得到一个数字后要看看之前有没有出现这个数。为了节省搜索时间，我们采用哈希表来存数每个小数位上的数字。还有一个小技巧，由于我们要算出小数每一位，采取的方法是每次把余数乘10，再除以除数，得到的商即为小数的下一位数字。等到新算出来的数字在之前出现过，则在循环开始出加左括号，结束处加右括号。
 '''
 
 class Solution:
