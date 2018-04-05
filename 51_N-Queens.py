e2aaaffbe9b7f3d2412b6e53ed0fbfe00ee79e64
Shirley <xueyi.fan27@gmail.com>
2018-04-05 10:41:13 -0400

    Update 51_N-Queens.py

--- 51_N-Queens.py
+++ 51_N-Queens.py
@@ -74,7 +74,43 @@ class Solution(object):
         
         return(False)
     
-    
+class Solution:
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        res = []
+        curr = [["."] * n  for _ in range(n)]
+        self._helpFun(res, n, 0, curr)
+        return(res)
+           
+    def _helpFun(self, res, n, index, curr):
+        if index == n:
+            res.append(["".join(i) for i in curr])
+            return
+        for j in range(n):
+            if self._isAttatched(index, j, curr, n):
+                continue
+            else:
+                curr[index][j] = "Q"
+                self._helpFun(res, n, index+1, curr)
+                curr[index][j] = "."
+        return
+
+    def _isAttatched(self, rowIndex, colIndex, curr, n):
+        #check column
+        for i in range(rowIndex):
+            if curr[i][colIndex] == "Q":
+                return(True)
+        
+        #check diagnal
+        for i in range(rowIndex):
+            for j in range(n):
+                if curr[i][j] == "Q" and abs(rowIndex-i) == abs(colIndex-j):
+                    return(True)
+        return(False)
+                
     
     
         
