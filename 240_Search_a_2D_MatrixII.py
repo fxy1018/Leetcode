a5d34618cb784d4de4cc8de184b69a44eef32bac
Shirley <xueyi.fan27@gmail.com>
2018-04-05 09:25:24 -0400

    Create 240_Search_a_2D_MatrixII.py

---
+++ 240_Search_a_2D_MatrixII.py
@@ -0,0 +1,40 @@
+'''
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+For example,
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+
+'''
+
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        #method1: brute-force o(n*m)
+        if not matrix:
+            return(False)
+        
+        row = len(matrix)
+        col = len(matrix[0])
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == target:
+                    return(True)
+        return(False)
