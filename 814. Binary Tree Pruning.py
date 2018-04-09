04578fc1af6a177e23f3cca6f13582dfcd5c4bbe
Shirley <xueyi.fan27@gmail.com>
2018-04-09 09:59:47 -0400

    Create 814. Binary Tree Pruning.py

---
+++ 814. Binary Tree Pruning.py
@@ -0,0 +1,67 @@
+'''
+We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.
+
+Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+(Recall that the subtree of a node X is X, plus every node that is a descendant of X.)
+Example 1:
+Input: [1,null,0,0,1]
+Output: [1,null,0,null,1]
+ 
+Explanation: 
+Only the red nodes satisfy the property "every subtree not containing a 1".
+The diagram on the right represents the answer.
+
+
+Example 2:
+Input: [1,0,1,0,0,0,1]
+Output: [1,null,1,null,1]
+
+
+
+Example 3:
+Input: [1,1,0,1,1,0,1,0]
+Output: [1,1,0,1,1,null,1]
+
+
+
+Note:
+
+The binary tree will have at most 100 nodes.
+The value of each node will only be 0 or 1.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return(None)
+        if self._hasOne(root):
+            return(root)
+        else:
+            return(None)
+        
+        
+    def _hasOne(self, node):
+        if not node:
+            return(False)
+        left = self._hasOne(node.left)
+        right = self._hasOne(node.right)
+        if not left:
+            node.left = None
+        if not right:
+            node.right = None
+        if node.val == 0 and not left and not right:
+            return(False)
+        else:
+            return(True)
