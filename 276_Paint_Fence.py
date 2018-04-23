469609e1c5cef3b87e75af2a476d1fdaf8a52643
Shirley <xueyi.fan27@gmail.com>
2018-04-23 12:54:53 -0400

    Update 276_Paint_Fence.py

--- 276_Paint_Fence.py
+++ 276_Paint_Fence.py
@@ -15,4 +15,26 @@ Created on Feb 1, 2017
 
 """
 
-
+class Solution:
+    """
+    @param n: non-negative integer, n posts
+    @param k: non-negative integer, k colors
+    @return: an integer, the total number of ways
+    """
+    #dp[n][k] = dp[n-2]*(k-1) + dp[n-1] * (k-1)
+    def numWays(self, n, k):
+        # write your code here
+        if n == 0 or k == 0:
+            return(0)
+        if n == 1:
+            return(k)
+        if n == 2:
+            return(k*k)
+        dp = [0 for _ in range(n)]
+        dp[0] = k
+        dp[1] = k *k
+        for i in range(2, n):
+            dp[i] = (k-1) * (dp[i-2] + dp[i-1]) 
+        return(dp[-1])
+        
+        
