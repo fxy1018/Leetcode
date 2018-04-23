619433bcce74f9ffc4fa24544f581e15a4d57934
Shirley <xueyi.fan27@gmail.com>
2018-04-23 13:43:27 -0400

    Update 265_PaintHouseII.py

--- 265_PaintHouseII.py
+++ 265_PaintHouseII.py
@@ -16,3 +16,86 @@ Could you solve it in O(nk)?
 
 '''
 
+class Solution:
+    """
+    @param costs: n x k cost matrix
+    @return: an integer, the minimum cost to paint all houses
+    """
+   #time limit exceeded
+    def minCostII(self, costs):
+        # write your code here
+        if not costs:
+            return(0)
+            
+        house = len(costs)
+        color = len(costs[0])
+        
+        #create dp 
+        dp = [[0] * color for _ in range(house)]
+        dp[0] = costs[0]
+        
+        for i in range(1, house):
+            for j in range(color):
+                last = []
+                for z in range(color):
+                    if z != j:
+                        last.append(dp[i-1][z])
+                dp[i][j] = min(last) + costs[i][j]
+        return(min(dp[-1]))
+class Solution:
+    """
+    @param costs: n x k cost matrix
+    @return: an integer, the minimum cost to paint all houses
+    """
+    #similar strategy as paint house 1 but use two variables to record the last two mins for each row
+    def minCostII(self, costs):
+        # write your code here
+        if not costs:
+            return(0)
+            
+        house = len(costs)
+        color = len(costs[0])
+        
+        #create dp 
+        dp = [[0] * color for _ in range(house)]
+        dp[0] = costs[0]
+        if costs[0][0] <= costs[0][1]:
+            min1 = 0
+            min2 = 1
+        else:
+            min1 = 1
+            min2 = 0
+            
+        for i in range(2, color):
+            if costs[0][i] < costs[0][min1]:
+                min2 = min1
+                min1 = i
+                
+            elif costs[0][i] == costs[0][min1] or costs[0][i] < costs[0][min2]:
+                min2 = i
+            
+        for i in range(1, house):
+            last1 = -1
+            last2 = -1
+            for j in range(color):
+                if j == min1:
+                    dp[i][j] = dp[i-1][min2] + costs[i][j]
+                else:
+                    dp[i][j] = dp[i-1][min1] + costs[i][j]
+                
+                if last1 == -1:
+                    last1 = j
+                else:
+                    if dp[i][j] < dp[i][last1]:
+                        last2 = last1
+                        last1 = j
+                    elif dp[i][j] == dp[i][last1]:
+                        last2 = j
+                    elif last2 == -1 or dp[i][j] < dp[i][last2]:
+                        last2 = j
+            min1 = last1
+            min2 = last2
+                
+        return(min(dp[-1]))
+       
+       
