81212ce0417f65dc89729f7061e6444e3d56a393
Shirley <xueyi.fan27@gmail.com>
2018-04-23 11:02:48 -0400

    Update 256_Paint_House.py

--- 256_Paint_House.py
+++ 256_Paint_House.py
@@ -38,5 +38,49 @@ class Solution(object):
             
         return(min(cost_total[n][0], cost_total[n][1], cost_total[n][2]))    
         
+class Solution:
+    """
+    @param costs: n x 3 cost matrix
+    @return: An integer, the minimum cost to paint all houses
+    """
+    def minCost(self, costs):
+        # write your code here
+        #0:RED, 2:GREEN, 1:BLUE
         
-
+        if not costs:
+            return(0)
+            
+        house = len(costs)
+        dp = [[0] * 3 for _ in range(house)]
+        dp[0] = costs[0]
+        
+        for i in range(1, house):
+            dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
+            dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
+            dp[i][2] = min(dp[i-1][1], dp[i-1][0]) + costs[i][2]
+        
+        return(min(dp[-1]))
+                
+class Solution:
+    """
+    @param costs: n x 3 cost matrix
+    @return: An integer, the minimum cost to paint all houses
+    """
+    #compress space
+    def minCost(self, costs):
+        # write your code here
+        #0:RED, 2:GREEN, 1:BLUE
+        
+        if not costs:
+            return(0)
+            
+        house = len(costs)
+        dp = costs[0]
+        for i in range(1, house):
+            t0 = min(dp[1], dp[2]) + costs[i][0]
+            t1 = min(dp[0], dp[2]) + costs[i][1]
+            t2 = min(dp[1], dp[0]) + costs[i][2]
+            dp = [t0,t1,t2]
+            
+        return(min(dp))
+        
