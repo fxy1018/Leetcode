a958637c0a3207754f865f2ad149815885244927
Shirley <xueyi.fan27@gmail.com>
2018-04-26 13:33:17 -0400

    Create 20_ValidParentheses.py

---
+++ 20_ValidParentheses.py
@@ -0,0 +1,56 @@
+'''
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+'''
+
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if not s:
+            return(True)
+        
+        stack = []
+        for c in s:
+            if c in "([{":
+                stack.append(c)
+            else:
+                if not stack:
+                    return(False)
+                elif c == ")" and stack[-1] != "(":
+                    return(False)
+                elif c == "]" and stack[-1] != "[":
+                    return(False)
+                elif c == "}" and stack[-1] != "{":
+                    return(False)
+                stack.pop()
+        return(len(stack)==0)
