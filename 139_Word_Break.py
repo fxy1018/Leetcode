1a595f7008b720bc5653edf9fc8c44e331356972
Shirley <xueyi.fan27@gmail.com>
2018-05-11 10:10:13 -0400

    Update 139_Word_Break.py

--- 139_Word_Break.py
+++ 139_Word_Break.py
@@ -79,3 +79,20 @@ class Solution(object):
                     dp[i+1] = True
                     break
         return(dp[-1])
+class Solution:
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        wordDict = set(wordDict)
+        dp = [False for i in range(len(s)+1)]
+        dp[0] = True
+        
+        for i in range(1,len(s)+1):
+            for j in range(0, i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        return(dp[-1])
