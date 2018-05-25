42608cba9af84d8bffca16dc2f0eb39816c8073c
Shirley <xueyi.fan27@gmail.com>
2018-05-25 10:18:36 -0400

    Update 173_BinarySearchTreeIterator.py

--- 173_BinarySearchTreeIterator.py
+++ 173_BinarySearchTreeIterator.py
@@ -48,3 +48,56 @@ class BSTIterator(object):
 # Your BSTIterator will be called like this:
 # i, v = BSTIterator(root), []
 # while i.hasNext(): v.append(i.next())
+
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+Example of iterate a tree:
+iterator = BSTIterator(root)
+while iterator.hasNext():
+    node = iterator.next()
+    do something for node 
+"""
+
+#space: O(n)
+class BSTIterator:
+    """
+    @param: root: The root of binary tree.
+    """
+    def __init__(self, root):
+        # do intialization if necessary
+        self.tree = []
+        self.stack = []
+        while root or self.stack:
+            if root:
+                self.stack.append(root)
+                root = root.left
+            else:
+                root = self.stack.pop()
+                self.tree.append(root)
+                root = root.right
+            
+        self.tree = self.tree[::-1]
+    """
+    @return: True if there has next node, or false
+    """
+    def hasNext(self, ):
+        # write your code here
+        if self.tree:
+            return(True)
+        else:
+            return(False)
+    """
+    @return: return next node
+    """
+    def next(self, ):
+        # write your code here
+        if self.tree:
+            return(self.tree.pop())
+        else:
+            return()
