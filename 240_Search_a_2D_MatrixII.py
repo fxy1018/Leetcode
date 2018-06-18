f6d35f6f16952b090059b0c5c64dce29f4851e4d
Shirley <xueyi.fan27@gmail.com>
2018-06-18 10:55:37 -0400

    Update 240_Search_a_2D_MatrixII.py

--- 240_Search_a_2D_MatrixII.py
+++ 240_Search_a_2D_MatrixII.py
@@ -38,3 +38,73 @@ class Solution:
                 if matrix[i][j] == target:
                     return(True)
         return(False)
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+
+        if not matrix:
+            return(False)
+        
+        row = len(matrix)
+        col = len(matrix[0])
+        left = top = 0
+        right = col-1
+        bottom = row -1
+        
+        while left < right-1 and top < bottom-1:
+            colMid = (left + right)//2
+            rowMid = (top + bottom)//2
+            if matrix[rowMid][colMid] == target:
+                return(True)
+            if matrix[rowMid][colMid] > target:
+                if matrix[colMid][top] <= target:
+                    right = colMid 
+                else:
+                    right = colMid-1
+                if matrix[rowMid][left] <= target:
+                    bottom = rowMid
+                else:
+                    bottom = rowMid-1
+                    
+            if matrix[rowMid][colMid] < target:
+                if matrix[colMid][bottom] >= target:
+                    left = colMid 
+                else:
+                    left = colMid+1
+                if matrix[rowMid][right] >= target:
+                    top = rowMid
+                else:
+                    top = rowMid+1
+ 
+        return(False)
+
+#time: O(m+n)
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        
+        if not matrix or not matrix[0]:
+            return(False)
+        #start from the righttop corner
+        row = len(matrix)
+        col = len(matrix[0])
+        top = 0
+        right = col-1
+        
+        while top < row and right >= 0:
+            if matrix[top][right] == target:
+                return(True)
+            elif matrix[top][right] > target:
+                right -=1
+            else:
+                top += 1
+ 
+        return(False)
